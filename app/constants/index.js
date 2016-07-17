15b6f1ea82f261894be36e6bd9816924d37e27dc
François Barrailla <francois.barrailla@gmail.com>
2016-07-17 20:08:20 +0200

    static version

--- app/constants/index.js
+++
@@ -1,5 +0,0 @@
-export const POKEAPI_URL = './pokemons.json'
-
-export const POKEAPI_DETAILS_URL = 'https://pokeapi.co/api/v2/pokemon'
-
-export const PICTURE_URL = 'http://pokeapi.co/media/sprites/pokemon'
